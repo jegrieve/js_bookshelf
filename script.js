163ec1360bf47306a055156168fae0b98b6cc329
James Grieve <pocojames@hotmail.com>
2020-10-04 01:48:24 -0700

    add updatelibrary, togglereadstatus and removebook functions

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let myLibrary = [];
 
+const addBtn = document.querySelector('#add-btn');
+const addForm = document.querySelector('#add-form');
+const submitBtn = document.querySelector('#submit-btn');
+
+addBtn.addEventListener("click", function() {
+    addForm.hidden = false;
+    addBtn.hidden = true;
+})
+
+submitBtn.addEventListener("click", addBookToLibrary);
+submitBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+})
+
+
 function Book(title, author, pages, read) { 
     this.title = title;
     this.author = author;
@@ -16,4 +31,48 @@ function Book(title, author, pages, read) {
       this.read = !this.read;
     };
   };
+
+  function addBookToLibrary() {
+    const title = document.querySelector('#title-input').value;
+    const author = document.querySelector('#author-input').value;
+    const pages = document.querySelector('#pages-input').value;
+    const read = document.querySelector('#read-input').checked;
+    addForm.reset();
+    addForm.hidden = true;
+    addBtn.hidden = false;
+    myLibrary.push(new Book(title, author, pages, read));
+    updateLibrary();
+  }
+
+  function updateLibrary() {
+    const bookshelf = document.querySelector('#bookshelf');
+    bookshelf.innerHTML = '';
+    for (const [index, book] of myLibrary.entries()) { 
+      const bookEntry = document.createElement('li'); 
+      this.book = book; 
+      if (book.read) { 
+        bookEntry.classList.add('read');
+        bookEntry.innerHTML = `${book.info()} <button onclick="toggleReadStatus(book)">Mark unread</button>`;
+      } else { 
+        bookEntry.classList.add('unread');
+        bookEntry.innerHTML = `${book.info()} <button onclick="toggleReadStatus(book)">Mark read</button>`;
+      }
+      bookEntry.innerHTML += ` <button onclick="removeBook(${index})">Remove</button>`;
+      bookshelf.appendChild(bookEntry);
+    }
+  }
+  
+  function toggleReadStatus(book) {
+    book.toggleRead();
+    updateLibrary();
+  }
+  
+  function removeBook(index) {
+    if (index > -1) {
+      myLibrary.splice(index, 1);
+    }
+    updateLibrary();
+  }
+  
+
 
